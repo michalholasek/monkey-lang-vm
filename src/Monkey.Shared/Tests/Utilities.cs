7a18520d8447ecc1163f28a1343dd1446d84fbb7
Michal Holasek <holasek.michal@hotmail.com>
2019-04-12 01:37:28 +0200

    chore: Remove unnecessary AreDeeplyEqual<T> method

--- src/Monkey.Shared/Tests/Utilities.cs
+++ src/Monkey.Shared/Tests/Utilities.cs
@@ -9,24 +9,6 @@ namespace Monkey.Tests.Utilities
 {
     public static class Assert
     {
-        public static void AreDeeplyEqual<T>(List<T> a, List<T> b)
-        {
-            A.AreEqual(a.Count, b.Count);
-
-            for (var i = 0; i < a.Count; i++)
-            {
-                var props = a[i].GetType().GetProperties();
-
-                foreach (var prop in props)
-                {
-                    A.AreEqual(
-                        a[i].GetType().GetProperty(prop.Name).GetValue(a[i]),
-                        b[i].GetType().GetProperty(prop.Name).GetValue(b[i])
-                    );
-                }   
-            }
-        }
-
         public static void AreDeeplyEqual(object a, object b)
         {
             A.AreEqual(JsonConvert.SerializeObject(a), JsonConvert.SerializeObject(b));
