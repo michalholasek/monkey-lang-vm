a89b96a2e0cb7f67991e680d9dcb8dbde3a0dfe2
Michal Holasek <holasek.michal@hotmail.com>
2019-04-12 18:38:58 +0200

    feat(REPL): Add initial REPL project skeleton

---
+++ src/Monkey.Repl/Repl.cs
@@ -0,0 +1,50 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace Monkey.Repl
+{
+    class Program
+    {
+        private static List<string> commands;
+
+        static Program()
+        {
+            commands = new List<string>();
+        }
+
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Welcome to monkey-lang REPL!");
+
+            var condition = true;
+
+            while (condition)
+            {
+                Promt();
+                condition = HandleCommand(Console.ReadLine());
+            }
+
+            Console.WriteLine("Exiting monkey-lang REPL...");
+        }
+
+        private static bool HandleCommand(string command)
+        {
+            commands.Add(command);
+
+            if (commands.Count == 1 && (command == "q" || command == "quit"))
+            {
+                return false;
+            }
+
+            return true;
+        }
+
+        private static void Promt()
+        {
+            if (commands.Count == 0)
+            {
+                Console.Write("> ");
+            }
+        }
+    }
+}
