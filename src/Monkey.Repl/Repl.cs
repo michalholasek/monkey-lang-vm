ef20fa623d855554508cd00d48dcf140b81a9d71
Michal Holasek <holasek.michal@hotmail.com>
2019-04-16 21:06:07 +0200

    feat(REPL): Add R(ead) E(valuate) P(rint) L(oop)

--- src/Monkey.Repl/Repl.cs
+++ src/Monkey.Repl/Repl.cs
@@ -1,15 +1,28 @@
 ﻿using System;
 using System.Collections.Generic;
 
+using Monkey.Shared.Scanner;
+using Monkey.Shared.Parser;
+using Monkey.Shared.Evaluator;
+using Environment = Monkey.Shared.Evaluator.Environment;
+
 namespace Monkey.Repl
 {
-    class Program
+    class Repl
     {
         private static List<string> commands;
+        private static Environment env;
+        private static Scanner scanner;
+        private static Parser parser;
+        private static Evaluator evaluator;
 
-        static Program()
+        static Repl()
         {
             commands = new List<string>();
+            env = new Environment();
+            scanner = new Scanner();
+            parser = new Parser();
+            evaluator = new Evaluator();
         }
 
         static void Main(string[] args)
@@ -29,7 +42,20 @@ namespace Monkey.Repl
 
         private static bool HandleCommand(string command)
         {
-            commands.Add(command);
+            if (command == String.Empty && commands.Count > 0)
+            {
+                var source = string.Join(" ", commands);
+                var result = evaluator.Evaluate(parser.Parse(scanner.Scan(source)), env);
+
+                commands.Clear();
+
+                Console.SetCursorPosition(Console.CursorLeft, Console.CursorTop - 1);
+                Console.WriteLine(result.Value.ToString());
+            }
+            else
+            {
+                commands.Add(command);
+            }
 
             if (commands.Count == 1 && (command == "q" || command == "quit"))
             {
