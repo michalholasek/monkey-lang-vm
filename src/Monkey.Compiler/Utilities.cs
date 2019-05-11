4bf1a142195b5b6c64a939b73899e126647b51b4
Michal Holasek <holasek.michal@hotmail.com>
2019-05-11 23:41:30 +0200

    fix(Compiler): Make ReplaceInstruction work for any length

--- src/Monkey.Compiler/Utilities.cs
+++ src/Monkey.Compiler/Utilities.cs
@@ -8,6 +8,7 @@ namespace Monkey
     {
         private CompilerState RemoveLastPopInstruction(CompilerState previousState)
         {
+            // (3) Opcode.Pop
             var position = previousState.Instructions.LastIndexOf(3);
             previousState.Instructions.RemoveAt(position);
 
@@ -19,7 +20,7 @@ namespace Monkey
 
         private CompilerState ReplaceInstruction(int position, List<byte> instruction, CompilerState previousState)
         {
-            previousState.Instructions.RemoveRange(position, 3);
+            previousState.Instructions.RemoveRange(position, instruction.Count);
             previousState.Instructions.InsertRange(position, instruction);
             return previousState;
         }
